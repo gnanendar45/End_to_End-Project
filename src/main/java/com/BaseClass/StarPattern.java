806d183a1550a018c776f7d5c9e01bcb1f1a7633
admin <admin@DESKTOP-9V7FSPQ>
2024-07-20 11:34:42 +0530

    im creating the one star class using loops statements

---
+++ src/main/java/com/BaseClass/StarPattern.java
@@ -0,0 +1,42 @@
+package com.BaseClass;
+
+public class StarPattern {
+	
+	public static void main(String[] args) {
+		/*for(int i=1; i<=4; i++) {
+			for(int j=1; j<=4; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}*/
+
+	
+	//another pattern
+	
+	/*for(int i=1; i<=4; i++) { //first line lo 4stars ni indicate chestadi
+		for(int j=1; j<=i; j++) { // upto four rows one star upto four rows four stars
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+
+}*/
+		
+	/*
+	 * for(int i=4; i>=1; i--) { for(int j=1; j<=i; j++) { System.out.print("*"); }
+	 * System.out.println(); }
+	 */
+		
+		//Even number and hard numbers
+		
+		/*
+		 * for(int i=1; i<=10; i++) { if(i%2==0) { System.out.println(i); } }
+		 * 
+		 * for(int j=1; j<=10; j++) { if(j%2!=0) { System.out.println(j); } }
+		 */
+		
+	}
+}
+
+
